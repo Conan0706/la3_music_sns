f330e123b8583894f87674d0c9d05504d26b886f
Conan0706 <s1f101903007@iniad.org>
2022-02-22 17:25:07 +0000

    サインアップ、サインイン(画像登録のぞく)

--- app.rb
+++ app.rb
@@ -4,6 +4,44 @@ require 'sinatra/reloader' if development?
 require './models'
 require 'dotenv/load'
 
+enable :sessions
+
+helpers do
+    def  current_user
+        User.find_by(id :session[:user])
+    end
+end
+
+
 get '/' do
     erb :index
+end
+
+get "/signup" do
+    erb :sign_up
+end
+
+get "/signin" do
+    erb :search
+end
+
+post "/signup" do
+    @user = User.create(
+        name: params[:name],
+        password: params[:password], 
+        password_confirmation: params[:password_confirmation]
+    )
+    
+    if @user.persisted?
+        session[:user] = @user.id
+    end
+    redirect "/"
+end
+
+post "/signin" do
+    user=User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+    end
+    redirect "/signin"
 end
